50911dcb7f7229146b89a18d1078d8a5cf11ab34
7015997-hash <7015997@spsd.sk.ca>
2026-04-27 12:57:47 -0600

    input letters
    Downloaded fonts and input the letters.

--- sketch.js
+++ sketch.js
@@ -6,8 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+let alpahbets= ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let letter;
+let font;
+function preload(){
+  font = loadFont("Borsha-Italic.ttf");
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  letter = random(alphabets);
+  print(letter);
 }
 
 function draw() {
