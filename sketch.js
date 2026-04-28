603721b0fe6dfff789648df7669efe9667369f8d
7015997-hash <7015997@spsd.sk.ca>
2026-04-28 15:24:46 -0600

    made the class
    Made class for the alphabets.

--- sketch.js
+++ sketch.js
@@ -1,34 +1,48 @@
 // Project Title
-// Your Name
+// Mobashira
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Letter{
+  constructor(x,y){
+    this.alphabets = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+      "N","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    this.x = x;
+    this.y = y;
+    this.letter = random(this.alphabets);
+    this.angle = random(360);
+  }
+  dispaly(){
+    translate(width/2,height/2);
+    rotate(angle);
+    textFont(font);
+    textSize(50);
+    text(letter,0,0);
+    
+  }
+}
 
 
-let alpahbets= ["A","B","C","D","E","F","G","H","I","J","K","L","M",
-  "N","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let letter;
 let font;
 function preload(){
-  font = loadFont("Borsha-Italic.ttf");
+  font = loadFont("Borscha-Italic.ttf");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  letter = random(alpahbets);
+  letter = random(this.alpahbets);
   angleMode(DEGREES);
 
 }
 
 function draw() {
   background(220);
-  let angle = map(mouseX, 0, width, 0,360);
-  translate(width/2,height/2);
-  rotate(angle);
-  textFont(font);
-  textSize(50);
-  text(letter,0,0);
+
+
+  
 
   // square(mouseX,mouseY,50);
 }
