7ace88c4a83f522b20f6bb41bf67e86a986534a3
7015997-hash <7015997@spsd.sk.ca>
2026-06-03 15:19:25 -0600

    the logic of the animation
    Wrote down the logic of the animation after sorting out html and css I can put that up.

--- sketch.js
+++ sketch.js
@@ -130,6 +130,8 @@ function setup() {
 
 
   // // Animation game
+
+
   // centerX = width/2;
   // centerY = height/2;
   // for (let i = 0; i < nodes; i++) {
@@ -139,6 +141,13 @@ function setup() {
   //   nodeY[i] = 0;
   //   angle[i] = 0;
   // }
+//   for (let i = 0; i < nodes; i++) {
+//     frequency[i] = random(5, 12);
+//   }
+
+//   noStroke();
+//   angleMode(DEGREES);
+// }
 
 }
 
@@ -232,8 +241,67 @@ function draw() {
     }
 
   }
+
+  // background(0, 50);
+
+  // // Draw and move the shape
+  // drawShape();
+  // moveShape();
   
 }
+
+
+function drawShape() {
+  // Calculate node starting locations
+  for (let i = 0; i < nodes; i++) {
+    nodeStartX[i] = centerX + cos(rotAngle) * radius;
+    nodeStartY[i] = centerY + sin(rotAngle) * radius;
+    rotAngle += 360.0 / nodes;
+  }
+
+  // Draw the polygon
+
+  curveTightness(organicConstant);
+  let shapeColor = lerpColor(color('red'), color('yellow'), organicConstant);
+  fill(shapeColor);
+
+  beginShape();
+  for (let i = 0; i < nodes; i++) {
+    curveVertex(nodeX[i], nodeY[i]);
+  }
+  endShape(CLOSE);
+}
+
+// function moveShape() {
+//   // Move center point
+//   deltaX = mouseX - centerX;
+//   deltaY = mouseY - centerY;
+
+//   // Create springing effect
+//   deltaX *= springing;
+//   deltaY *= springing;
+//   accelX += deltaX;
+//   accelY += deltaY;
+
+//   // Move center
+//   centerX += accelX;
+//   centerY += accelY;
+
+//   // Slow down springing
+//   accelX *= damping;
+//   accelY *= damping;
+
+//   // Change curve tightness based on the overall acceleration;
+//   // use abs() to avoid dependence on direction of acceleration
+//   organicConstant = 1 - (abs(accelX) + abs(accelY)) * 0.1;
+
+//   // Move nodes
+//   for (let i = 0; i < nodes; i++) {
+//     nodeX[i] = nodeStartX[i] + sin(angle[i]) * (accelX * 2);
+//     nodeY[i] = nodeStartY[i] + sin(angle[i]) * (accelY * 2);
+//     angle[i] += frequency[i];
+//   }
+// }
 
 //  Changing all of the states.
 
